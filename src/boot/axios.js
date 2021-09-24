03308e4b0403e073e3a8aa67a8aad6355f98c8fe
Ninja <lininja.tw@gmail.com>
2021-09-24 23:26:31 +0800

    feat: Temporarily use nas api directly

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -4,7 +4,8 @@ import axios from 'axios';
 vue.prototype.$axios = axios;
 // ^ ^ ^ this will allow you to use this.$axios
 //       so you won't necessarily have to import axios in each vue file
-const api = axios.create({ baseURL: process.env.APP_API });
+// const api = axios.create({ baseURL: process.env.APP_API });
+const api = axios.create({ baseURL: 'http://lininja.asuscomm.com:3000' });
 
 vue.prototype.$api = api;
 // ^ ^ ^ this will allow you to use this.$api
